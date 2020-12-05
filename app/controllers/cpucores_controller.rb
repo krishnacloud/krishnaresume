89693a5e23ca5d7e2f133690868da2d5880c36a4
krishna crypt <kvenk@outlook.com>
2020-12-05 11:34:22 -0500

    type your commit message

--- app/controllers/cpucores_controller.rb
+++ app/controllers/cpucores_controller.rb
@@ -1,4 +1,8 @@
 class CpucoresController < ApplicationController
 	def home
+		require 'net/http'
+		
+		@symbol = params[:sym]
+		@url  =  'https://docs.microsoft.com/en-us/azure/virtual-machines/acu'
 	end
 end
